1a5a2dfa78f7790ca01765dda1cd40b2df97df94
Chris Aubuchon <aubuchcl@gmail.com>
2022-12-01 13:52:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("Version Three");
+  response.end("Version FROM PIPELINE WITH CUSTOM BRANCH");
   console.log("Got a connection");
 });
 server.listen(80, "::0");
